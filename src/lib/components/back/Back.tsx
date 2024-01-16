4df23e2929e4095db22577ebe8a4e96355864b2b
tms-5 <tms5@cin.ufpe.br>
2024-01-16 17:37:39 -0300

    Add Back component with styles and functionality

---
+++ src/lib/components/back/Back.tsx
@@ -0,0 +1,13 @@
+"use client";
+import ArrowBack from "../../assets/icon/arrow_back.png";
+import Image from "next/image";
+import './Back.scss';
+
+export default function Back(props: { text: string; onClick: () => void }) {
+  return (
+    <div className="d-flex back-component" onClick={props.onClick}>
+      <Image src={ArrowBack} alt="ArrowBack" />
+      <p className="c-gray-50 ml-1r">{props.text}</p>
+    </div>
+  );
+}
