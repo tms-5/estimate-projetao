368a7727868e4ca187ef13b7fc91adef32ce1a28
tms-5 <tms5@cin.ufpe.br>
2024-01-16 17:37:21 -0300

    Add new interfaces and enums

--- src/store/definitions.ts
+++ src/store/definitions.ts
@@ -1,4 +1,25 @@
-export interface Usuario {}
+import { seniorityType } from "./enum";
+
+export interface Login {
+  email: string;
+  password: string;
+}
+
+export interface User {
+  name: string;
+  nickname: string;
+  email: string;
+  password: string;
+  seniority: seniorityType;
+  ocupation: string;
+  analist: boolean;
+}
+
+export interface Company {
+  name: string;
+  email: string;
+  password: string;
+}
 
 export interface Task {}
 
---
+++ src/store/enum.ts
@@ -0,0 +1,12 @@
+export enum seniorityType {
+  intern = "Estagiário",
+  jr = "Junior",
+  pl = "Pleno",
+  sr = "Sênior",
+  specialist = "Especialista",
+}
+
+export enum userType {
+  company = "Empresa",
+  developer = "Desenvolvedor",
+}
