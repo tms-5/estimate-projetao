59952827ce49d2b1470d659363fbd6e7690f2f06
tms-5 <tms5@cin.ufpe.br>
2024-01-16 17:37:33 -0300

    Add Input component and styles

---
+++ src/lib/components/input/Input.tsx
@@ -0,0 +1,25 @@
+import "./Input.scss";
+
+export default function Input(props: {
+  type: string;
+  class?: string;
+  placeholder?: string;
+  onChange?: (e: any) => void;
+  onBlur?: (e: any) => void;
+  value?: any;
+  disabled?: boolean;
+  name?: string;
+}) {
+  return (
+    <input
+      type={props.type}
+      name={props.name}
+      className={props.class}
+      placeholder={props.placeholder}
+      onChange={props.onChange}
+      onBlur={props.onBlur}
+      value={props.value}
+      disabled={props.disabled}
+    />
+  );
+}
