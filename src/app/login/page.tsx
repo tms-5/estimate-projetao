0472e4209e73723da34b1ef30af7b86864a30839
tms-5 <tms5@cin.ufpe.br>
2024-01-16 17:38:20 -0300

    Refactor login and register pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+import Login from "@/lib/components/appComponents/login/Login";
+import Background from "@/lib/components/appComponents/background/background";
+
+
 export default function Page() {
-    return <p>Login page</p>;
-  }
-  
+  return (
+    <div className="d-flex h-100">
+      <Login />
+      <Background />
+    </div>
+  );
+}
