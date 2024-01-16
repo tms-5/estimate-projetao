26a3ba8ad28e4291eaeb14465450d0a6b00ce100
tms-5 <tms5@cin.ufpe.br>
2024-01-16 17:37:07 -0300

    Add Button component with styles

---
+++ src/lib/components/button/Button.tsx
@@ -0,0 +1,14 @@
+import "./Button.scss";
+
+export default function Button(props: {
+  class?: string;
+  text?: any;
+  value?: any;
+  onClick?: () => void;
+}) {
+  return (
+    <button className={props.class} onClick={props.onClick} value={props.value}>
+      {props.text}
+    </button>
+  );
+}
