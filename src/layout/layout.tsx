7929f357ba7f232067199399b26e28c4dc540e71
Edu-p <tms5@cin.ufpe.br>
2024-02-04 11:54:27 -0300

    feat: Add layout component for authenticated users

---
+++ src/layout/layout.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useAuth } from "@/context/AuthContext";
+import Sidebar from "@/lib/components/sidebar/Sidebar";
+import { userType } from "@/store/enum";
+import { isAuthenticated } from "@/store/main";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function Layout(props: {
+  childrenDev: React.ReactNode;
+  childrenCompany: React.ReactNode;
+}) {
+  const { user } = useAuth();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (!isAuthenticated()) {
+      router.push("/login");
+    }
+  }, [router]);
+
+  useEffect(() => {
+    if (user === "" || user === null) {
+      setIsLoading(true);
+    } else {
+      setIsLoading(false);
+    }
+  }, [user]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      {isAuthenticated()
+        ? user &&
+          typeof user !== "string" && (
+            <Sidebar
+              user={user!}
+              backTo={"Voltar para Tasks"}
+              backToRoute="/tasks"
+            >
+              {user.level === userType.developer
+                ? props.childrenDev
+                : props.childrenCompany}
+            </Sidebar>
+          )
+        : null}
+    </>
+  );
+}
