5ebe9321ebc35c40b7a046437f0207af4befc370
Edu-p <tms5@cin.ufpe.br>
2024-02-04 11:55:03 -0300

    fix: Refactor DashboardStats component

--- src/lib/components/dashboardStats/dashboardStats.tsx
+++ src/lib/components/dashboardStats/dashboardStats.tsx
@@ -1,20 +1,35 @@
 "use client";
 
-type StatTypes = "regular" | "profit" | "balance";
+import { StatTypes } from "@/store/enum";
 
-export default function DashboardStats(props: {value: string, text: string, type: StatTypes, color: string}) {
+export default function DashboardStats(props: {
+  value: string;
+  text: string;
+  type: StatTypes;
+  color: string;
+}) {
   return (
-    <div className="b-8" style={{
-      border: `1px solid #BDBDBD`,
-      minWidth: "180px",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: "0 1rem",
-    }}>
-      <p className="f-1-5 fw-800" style={{color: props.color}}>{props.value}</p>
-      <p className="f-08 fw-500 c-black" style={{color: props.color, marginTop: -20}}>{props.text}</p>
+    <div
+      className="b-8"
+      style={{
+        border: `1px solid #BDBDBD`,
+        minWidth: "180px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "0 1rem",
+      }}
+    >
+      <p className="f-1-5 fw-800" style={{ color: props.color }}>
+        {props.value}
+      </p>
+      <p
+        className="f-08 fw-500 c-black"
+        style={{ color: props.color, marginTop: -20 }}
+      >
+        {props.text}
+      </p>
     </div>
   );
 }
