dbfc1d24d7aa2a0eb87a5ca778d3a246f8a5fa3a
Edu-p <tms5@cin.ufpe.br>
2024-02-04 11:55:18 -0300

    Refactor DashboardPage component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,52 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
-import { useAuth } from "../context/AuthContext";
-import Sidebar from "@/lib/components/sidebar/Sidebar";
-import { useRouter } from "next/router";
-import { isAuthenticated } from "@/store/main";
 import HomeDev from "@/lib/components/appComponents/home/HomeDev";
 import HomeCompany from "@/lib/components/appComponents/home/HomeCompany";
-import { userType } from "@/store/enum";
+import Layout from "@/layout/layout";
 
 function DashboardPage() {
-  const { user } = useAuth();
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    if (!isAuthenticated()) {
-      router.push("/login");
-    }
-  }, []);
-
-  useEffect(() => {
-    if (user === "" || user === null) {
-      setIsLoading(true);
-    } else {
-      setIsLoading(false);
-    }
-  }, [user]);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  return (
-    <>
-      {isAuthenticated()
-        ? user &&
-          typeof user !== "string" && (
-            <Sidebar user={user!}>
-              {user.level === userType.developer ? (
-                <HomeDev user={user} />
-              ) : (
-                <HomeCompany />
-              )}
-            </Sidebar>
-          )
-        : null}
-    </>
-  );
+  return <Layout childrenDev={<HomeDev />} childrenCompany={<HomeCompany />} />;
 }
 
 export default DashboardPage;
