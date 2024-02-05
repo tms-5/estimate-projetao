d27169560dbffb773470bba315b09f6f77c060ba
Edu-p <tms5@cin.ufpe.br>
2024-02-04 23:57:19 -0300

    Refactor SidebarItem component

--- src/lib/components/sidebar/SidebarItem.tsx
+++ src/lib/components/sidebar/SidebarItem.tsx
@@ -3,7 +3,8 @@ import { useRouter } from "next/router";
 import IconMenu from "../../assets/icon/dashboard_FILL1_wght400_GRAD0_opsz24 1.png";
 
 export default function SidebarItem(props: {
-  option: { name: string; path: string; search: boolean }, key: string;
+  option: { name: string; path: string; search: boolean };
+  itemId: number;
 }) {
   const router = useRouter();
   const isRouteActive = (path: string) => {
@@ -14,7 +15,6 @@ export default function SidebarItem(props: {
       className={`d-flex align-items-center mb-1 pl-1 pr-1 c-pointer ${
         isRouteActive(props.option.path) ? "active-item-sidebar" : ""
       }`}
-      key={props.key}
       onClick={() => router.push(props.option.path)}
     >
       <Image src={IconMenu} width={15} alt="Icon menu" />
