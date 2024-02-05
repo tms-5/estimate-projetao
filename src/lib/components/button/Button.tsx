d46aa72aaac3b5eae5caf11a00225bfbbe51e2ce
Edu-p <tms5@cin.ufpe.br>
2024-02-04 23:56:59 -0300

    Update Button component's onClick prop

--- src/lib/components/button/Button.tsx
+++ src/lib/components/button/Button.tsx
@@ -2,7 +2,7 @@ export default function Button(props: {
   class?: string;
   text?: any;
   value?: any;
-  onClick?: () => void;
+  onClick?: () => void | any;
 }) {
   return (
     <button className={props.class} onClick={props.onClick} value={props.value}>
