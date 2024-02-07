d288ea5cef5d4accb4d052c6ab4452578034e4d1
tms-5 <tms5@cin.ufpe.br>
2024-02-07 14:58:17 -0300

    Update DataTable component to include filter functionality

--- src/lib/components/dataTable/DataTable.tsx
+++ src/lib/components/dataTable/DataTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import DoubleArrow from "@/lib/assets/icon/double-arrow.png";
 
@@ -13,12 +13,15 @@ export default function DataTable({
   hiddenFilters,
   FilterComponent,
 }: {
-  headers: { name: string; value: string }[];
+  headers: { value: string; key: string }[];
   data: any[];
   hasFilters?: boolean;
   hiddenFilters?: boolean;
   FilterComponent?: React.ElementType<FilterProps>;
 }) {
+  const [showFilters, setShowFilters] = useState<boolean>(
+    hiddenFilters ?? false
+  );
   const [filterValues, setFilterValues] = useState<{ [key: string]: string }>(
     {}
   );
@@ -27,18 +30,13 @@ export default function DataTable({
     filter: React.ReactNode;
   }
 
-  // const handleFilterChange = (key: string, value: string) => {
-  //   setFilterValues((prev) => ({ ...prev, [key]: value }));
-  // };
+  const handleFilterChange = (key: string, value: string) => {
+    setFilterValues((prev) => ({ ...prev, [key]: value }));
+  };
 
-  function handleFilterChange(key: string, value: string) {
-    console.log(key, value);
-  }
-
-  // Filtra os dados com base nos valores dos filtros
   const filteredData = data.filter((row) =>
-    Object.entries(filterValues).every(([key, value]) =>
-      row[key].toString().toLowerCase().includes(value.toLowerCase())
+    Object.entries(filterValues).every(([key, filterValue]) =>
+      row[key].toString().toLowerCase().includes(filterValue.toLowerCase())
     )
   );
 
@@ -48,36 +46,26 @@ export default function DataTable({
 
   return (
     <>
+      {hasFilters && !showFilters && FilterComponent && (
+        <FilterComponent functionFilter={handleFilterChange} />
+      )}
       {hasFilters && (
         <>
-          {FilterComponent && (
-            <FilterComponent functionFilter={handleFilterChange} />
-          )}
           <div className="d-flex align-items-center mt-1r">
             <hr className="line-hidden-filter-table" />
-            <div className="circle-hidden-filter-table">
+            <div
+              className="circle-hidden-filter-table c-pointer"
+              onClick={() => setShowFilters(!showFilters)}
+            >
               <Image
                 src={DoubleArrow}
                 alt="Double arrow"
-                className="svg-to-blue"
+                className={`svg-to-blue ${showFilters ? "rotate-arrow" : ""}`}
                 width={15}
               />
             </div>
             <hr className="line-hidden-filter-table" />
           </div>
-          {/* <div className="filters-container">
-            {headers.map((header) => (
-              <input
-                key={header.value}
-                type="text"
-                placeholder={`Filtrar por ${header.name}`}
-                value={filterValues[header.value] || ""}
-                onChange={(e) =>
-                  handleFilterChange(header.value, e.target.value)
-                }
-              />
-            ))}
-          </div> */}
         </>
       )}
       <div className="table-container mt-1 mb-1r">
@@ -86,8 +74,8 @@ export default function DataTable({
           <thead>
             <tr>
               {headers.map((header) => (
-                <th key={header.value} className="f-07 text-start fw-500 pb-1">
-                  {header.name}
+                <th key={header.key} className="f-07 text-start fw-500 pb-1">
+                  {header.value}
                 </th>
               ))}
             </tr>
@@ -96,8 +84,10 @@ export default function DataTable({
             {filteredData.map((row: any, index: number) => (
               <tr key={index}>
                 {headers.map((header) => (
-                  <td key={header.value} className="f-07">
-                    {row[header.value]}
+                  <td key={header.key} className="f-07">
+                    {Array.isArray(row[header.key])
+                      ? row[header.key]?.join(", ")
+                      : row[header.key]}
                   </td>
                 ))}
               </tr>
