60a1753bd3f257148d043525fdf76ecf4a9c192f
paulaperazzo <mpps@cin.ufpe.br>
2024-02-07 18:08:03 -0300

    project table and project dev page done

--- src/lib/components/appComponents/projects/ProjectDev.tsx
+++ src/lib/components/appComponents/projects/ProjectDev.tsx
@@ -6,33 +6,62 @@ import TaskCard from "../../card/TaskCard";
 import DataTable from "../../dataTable/DataTable";
 import PageTitle from "../../pageTitle/PageTitle";
 
+type DataCardType = {
+  deadline: number;
+  header: string;
+  taskTotal: number;
+  taskDone: number;
+};
+
 export default function ProjectDev() {
     function onSearch(searchTerm: string) {
     console.log(searchTerm);
   }
+  
   let headers = [
     { name: "Nome", value: "name" },
-    { name: "Tecnologia", value: "tecnology" },
-    { name: "Biblioteca", value: "library" },
+    { name: "Data", value: "date" },
+    { name: "Status", value: "status" },
   ];
 
   let data = [
     {
       name: "Criar gráfico de pizza",
-      tecnology: ["ReactJs", "VueJs", "SvelteJs"],
-      library: ["D3-Scale", "D3"],
+      date: "05/10/2024",
+      status: "Finalizado",
     },
     {
       name: "Criar formulário",
-      tecnology: ["ReactJs"],
-      library: ["React forms", "Svelte forms"],
+      date: "05/10/2024",
+      status: "Finalizado",
     },
     {
       name: "Criar tabela",
-      tecnology: ["ReactJs", "SvelteJs"],
-      library: ["Svelte Components"],
+      date: "05/10/2024",
+      status: "Finalizado",
     },
   ];
+
+  let cardDataEx: DataCardType[] = [
+    {
+      deadline: 1,
+      header: "Projeto 1",
+      taskTotal: 10,
+      taskDone: 1
+    },
+    {
+      deadline: 2,
+      header: "Projeto 2",
+      taskDone: 2,
+      taskTotal: 10,
+    },
+    {
+      deadline: 3,
+      header: "Projeto 3",
+      taskDone: 3,
+      taskTotal: 10,
+    },
+  ]
 
   return (
     <>
@@ -55,13 +84,18 @@ export default function ProjectDev() {
       <hr style={{marginTop: '-16px', width: '4%', marginLeft: '8px', borderColor: '#1A3B7C', marginBottom: '24px'}} />
 
       <div style={{ display: 'flex', flexWrap: 'wrap',  gap: '16px'}}>
-        <ProjectCard deadline={7} header={'undefined'} tasksTotal={10} tasksDone={5}/>
-        <ProjectCard deadline={7} header={'undefined'} tasksTotal={10} tasksDone={5}/>
-        <ProjectCard deadline={7} header={'undefined'} tasksTotal={10} tasksDone={5}/>
-        <ProjectCard deadline={7} header={'undefined'} tasksTotal={10} tasksDone={5}/>
+        {cardDataEx.map((card: DataCardType) => (
+          <ProjectCard 
+            key={Math.random()} 
+            deadline={card.deadline} 
+            header={card.header} 
+            tasksTotal={card.taskTotal} 
+            tasksDone={card.taskDone}
+          />
+        ))}
       </div>
 
-      <div style={{ marginTop: '40px' }}>
+      <div style={{ marginTop: '80px' }}>
         <FilterNav 
           allProjects={<DataTable headers={headers} data={data} />} 
           finishedProjects={<DataTable headers={headers} data={data} />} 
