9971b75f0e0eabe08e5cc53fc77b0c62f0679a88
tms-5 <tms5@cin.ufpe.br>
2024-02-07 17:00:02 -0300

    Add newProject component

---
+++ src/pages/projects/newProject.tsx
@@ -0,0 +1,7 @@
+import Layout from "@/layout/layout";
+
+export default function NewProject() {
+  return (
+    <Layout childrenDev={<></>} childrenCompany={<></>} devHasntAccess={true} />
+  );
+}
