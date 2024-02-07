180989aa856820ecf946e51e47869eb8950b3e12
tms-5 <tms5@cin.ufpe.br>
2024-02-07 14:58:14 -0300

    Refactor TaskFilter component

--- src/lib/components/appComponents/task/Components/TaskFilter.tsx
+++ src/lib/components/appComponents/task/Components/TaskFilter.tsx
@@ -5,10 +5,9 @@ interface FilterProps {
 export const TaskFilter: React.FC<FilterProps> = ({ functionFilter }) => {
   return (
     <>
-      <button onClick={() => functionFilter("teste", "teste")}>aa</button>
       <div className="mt-1r" />
       <div className="d-flex mt-1r">
-        <div className="d-grid w-50">
+        <div className="d-grid w-50 pr-1">
           <div className="f-07 fw-500">Ação da task:</div>
           <select>
             <option>React</option>
@@ -16,7 +15,7 @@ export const TaskFilter: React.FC<FilterProps> = ({ functionFilter }) => {
             <option>Vue.js</option>
           </select>
         </div>
-        <div className="d-grid w-50">
+        <div className="d-grid w-50 pl-1">
           <div className="f-07 fw-500">Componente de manipulação:</div>
           <select>
             <option>React</option>
@@ -26,17 +25,19 @@ export const TaskFilter: React.FC<FilterProps> = ({ functionFilter }) => {
         </div>
       </div>
       <div className="d-flex mt-1r">
-        <div className="d-grid w-50">
+        <div className="d-grid w-50 pr-1">
           <div className="f-07 fw-500">Tecnologia:</div>
-          <select>
+          <select onChange={(e) => functionFilter("tecnology", e.target.value)}>
+            <option value={""}>Todos</option>
             <option>React</option>
             <option>Svelte</option>
             <option>Vue.js</option>
           </select>
         </div>
-        <div className="d-grid w-50">
+        <div className="d-grid w-50 pl-1">
           <div className="f-07 fw-500">Biblioteca:</div>
-          <select>
+          <select onChange={(e) => functionFilter("library", e.target.value)}>
+            <option value={""}>Todos</option>
             <option>React</option>
             <option>Svelte</option>
             <option>Vue.js</option>
