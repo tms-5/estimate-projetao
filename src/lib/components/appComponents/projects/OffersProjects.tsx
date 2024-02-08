f82ae9f04717472b1d82dfd8ef6f98578442a65c
paulaperazzo <mpps@cin.ufpe.br>
2024-02-08 11:25:29 -0300

    project offer cards done

--- src/lib/components/appComponents/projects/OffersProjects.tsx
+++ src/lib/components/appComponents/projects/OffersProjects.tsx
@@ -9,22 +9,22 @@ export default function OffersProjects() {
         <p>Novas Ofertas</p>
         <hr style={{marginTop: '-16px', width: '8%', marginLeft: '8px', borderColor: '#1A3B7C', marginBottom: '24px'}} />
         <div style={{ display: 'flex', flexWrap: 'wrap',  gap: '16px'}}>
-          <ProjectCard tasksDone={0} tasksTotal={0} />
-          <ProjectCard tasksDone={0} tasksTotal={0} />
+          <ProjectCard header={'proposta 1'} deadlineDate="17/10/2024" technology="NextJs" tasksTotal={2} />
+          <ProjectCard header={'proposta 2'} deadlineDate="17/10/2024" technology="NextJs" tasksTotal={2} />
         </div>
 
-        <p>Ofertas Antigas</p>
+        <p style={{ marginTop: '72px' }}>Ofertas Antigas</p>
         <hr style={{marginTop: '-16px', width: '8%', marginLeft: '8px', borderColor: '#1A3B7C', marginBottom: '24px'}} />
         <div style={{ display: 'flex', flexWrap: 'wrap',  gap: '16px'}}>
-          <ProjectCard tasksDone={0} tasksTotal={0} />
-          <ProjectCard tasksDone={0} tasksTotal={0} />
+          <ProjectCard header={'proposta 3'} deadlineDate="17/10/2024" technology="NextJs" tasksTotal={3} />
+          <ProjectCard header={'proposta 4'} deadlineDate="17/10/2024" technology="NextJs" tasksTotal={4} />
         </div>
 
-        <p>Ofertas Recusadas</p>
+        <p style={{ marginTop: '72px' }}>Ofertas Recusadas</p>
         <hr style={{marginTop: '-16px', width: '8%', marginLeft: '8px', borderColor: '#1A3B7C', marginBottom: '24px'}} />
-        <div style={{ display: 'flex', flexWrap: 'wrap',  gap: '16px'}}>
-          <ProjectCard tasksDone={0} tasksTotal={0} />
-          <ProjectCard tasksDone={0} tasksTotal={0} />
+        <div style={{ display: 'flex', flexWrap: 'wrap',  gap: '16px', marginBottom: '80px'}}>
+          <ProjectCard header={'proposta 5'} deadlineDate="17/10/2024" technology="NextJs" tasksTotal={5} />
+          <ProjectCard header={'proposta 6'} deadlineDate="17/10/2024" technology="NextJs" tasksTotal={6} />
         </div>
       </>
     ) 
--- src/lib/components/card/ProjectCard.tsx
+++ src/lib/components/card/ProjectCard.tsx
@@ -1,21 +1,55 @@
+import { useEffect, useState } from "react";
+
 export default function ProjectCard(props: {
   header?: React.ReactNode;
-  tasksDone: number;
-  tasksTotal: number;
+  tasksDone?: number;
+  tasksTotal?: number;
   deadline?: number;
   class?: string;
+  deadlineDate?: string;
+  technology?: string;
 }) {
+  const [percentage, setPercentage] = useState<number>(0);
 
-  const percentageDone = ((props.tasksDone / props.tasksTotal) * 100);
+  useEffect(() => {
+    if (props.tasksTotal && props.tasksDone) {
+      // const percentageDone = ((props.tasksDone / props.tasksTotal) * 100);
+      const percentageDone = (num1: number, num2: number) => {
+        const division = (num1 / num2) * 100;
+  
+        setPercentage(division);
+      }
+
+      percentageDone(props.tasksDone, props.tasksTotal);
+    }
+  }, []);
 
   return (
     <div className={props.class ?? "card-default"} style={{ width: '32%', borderTop: '16px solid #1C2664' }}>
       <h3 style={{ marginLeft: '16px' }}>#{props.header}</h3>
-      <p style={{ marginLeft: '16px', fontSize: '16px', color: '#757575'}}>{props.tasksDone}/{props.tasksTotal} finalizados</p>
-      <p style={{ marginLeft: '16px', fontSize: '16px', color: '#757575'}}>{props.deadline} dias restantes</p>
-      <div style={{ display: 'flex', justifyContent: "flex-end", marginTop: '-16px'}}>
-        <p style={{ fontSize: '16px', color: '#000', marginRight: '16px'}}>{percentageDone}%</p>
-      </div>
+      {props.deadline ? (
+          <>
+            <p style={{ marginLeft: '16px', fontSize: '16px', color: '#757575'}}>{props.tasksDone}/{props.tasksTotal} finalizados</p>
+            <p style={{ marginLeft: '16px', fontSize: '16px', color: '#757575'}}>{props.deadline} dias restantes</p>
+            <div style={{ display: 'flex', justifyContent: "flex-end", marginTop: '-16px'}}>
+              <p style={{ fontSize: '16px', color: '#000', marginRight: '16px'}}>{percentage}%</p>
+            </div>
+          </>
+        )
+      : (
+          <>
+            <p style={{ marginLeft: '16px', fontSize: '16px', color: '#757575', marginBottom: '-10px' }}>
+              <strong>Deadline: </strong> {props.deadlineDate}
+            </p>
+            <p style={{ marginLeft: '16px', fontSize: '16px', color: '#757575', marginBottom: '-10px' }}> 
+              <strong>Tecnoogia: </strong> {props.technology}
+            </p>
+            <p style={{ marginLeft: '16px', fontSize: '16px', color: '#757575' }}> 
+              <strong>Tasks: </strong> {props.tasksTotal}
+            </p>
+          </>
+        )
+      }
     </div>
   );
 }
