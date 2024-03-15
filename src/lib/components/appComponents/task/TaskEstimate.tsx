e7341b705b6d8e501a9b63bc0a92279eac0601be
paulaperazzo <mpps@cin.ufpe.br>
2024-03-15 17:55:26 -0300

    feat/dev-estimate: estimate page done

--- src/lib/components/appComponents/task/TaskEstimate.tsx
+++ src/lib/components/appComponents/task/TaskEstimate.tsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
 import Button from "../../button/Button";
-import Input from "../../input/Input";
 import PageTitle from "../../pageTitle/PageTitle";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { useRouter } from "next/router";
+
+const styleModal = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '1px solid #CDE1FF',
+  borderRadius: '8px',
+  boxShadow: 24,
+  p: 4,
+};
 
 const styles = {
   marginBottom: "8px",
@@ -18,13 +35,22 @@ const styles2 = {
 
 export default function TaskEstimate() {
   const [formData, setFormData] = useState({});
+  const [open, setOpen] = React.useState(false);
+  const router = useRouter();
+
+  const handleClose = () => setOpen(false);
 
   const handleInput = (e: string | number, field: string) => {
     setFormData({ ...formData, [field]: e });
   } 
 
   const handleClick = () => {
-    console.log('formData', formData);
+    setOpen(!open);
+  }
+
+  const handleGoToHome = () => {
+    setOpen(false);
+    router.push('/home');
   }
 
   return (
@@ -75,6 +101,26 @@ export default function TaskEstimate() {
           class="bg-teal-blue-100 c-white b-none no-filter p-1 w-8 fw-600"
           onClick={handleClick}
         />
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={styleModal}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Estimativa cadastrada!
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Sua estimativa foi cadastrada com sucesso! Em breve você receberá um feedback.
+            </Typography>
+            <Button
+              text={"Voltar para a tela inicial"}
+              class="bg-teal-blue-100 c-white b-none no-filter p-1 w-4 fw-300 mt-900"
+              onClick={handleGoToHome}
+            />
+          </Box>
+        </Modal>
       </div>
     </div>
   )
